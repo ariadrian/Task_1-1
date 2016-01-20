cd6e6b59797fdaf401462ff45f36e3b32e5f68aa
Ari Adrian Ibrahim <ariadrian.i@gmail.com>
2016-01-20 08:00:18 +0700

    Update IndexPoint.java
    Ari Adrian Ibrahim
    1301142301
    IF-38-01

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,35 @@
 
+import java.util.Scanner;
+
+public class IndexPoint {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double x;
+        double y;
+        double z;
+        double w;
+        System.out.print("Mid-term Score : ");
+        x = input.nextInt();
+        System.out.print("End-term Score : ");
+        y = input.nextInt();
+        System.out.print("Quiz score : ");
+        z = input.nextInt();
+        w = x/100*35 + y/100*40 + z/100*25;
+        if (w >= 85) 
+                System.out.println("Grade : Excellent");
+        else if ((w >= 75) && (w <= 84))
+                System.out.println("Grade : Very Good");
+        else if ((w >= 65) && (w <= 74))
+                System.out.println("Grade : Good");        
+        else if ((w >= 50) && (w <= 64))
+                System.out.println("Grade : Accepted");
+        else if ((w >= 0) && (w <= 49))
+                System.out.println("Grade : Failed");        
+    }
+    
+}
+
+//Ari Adrian Ibrahim
+//1301142301
+//if=38=01
