8bf0e492f52d830e839c14b1f73fb283ab7aa6fa
Ari Adrian Ibrahim <ariadrian.i@gmail.com>
2016-01-20 07:51:52 +0700

    Update PrimeNumber.java
    Ari Adrian Ibrahim
    1301142301
    IF-38-01

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class PrimeNumber{
+	
+
+ public static void main(String[]args){
+ int d,a;
+ a=0;
+ Scanner inputan = new Scanner(System.in);
+ System.out.println("Masukkan Angka : ");
+ d = inputan.nextInt();
+ for (int i=1; i<=d; i++){
+	 if (d % i == 0) {
+		a=a+1; 
+	 } 
+ }  
+ if (a==2) {
+	 System.out.println("Bilangan Prima");
+ }
+ else {
+	 System.out.println("Bukan Bilangan Prima");
+ }
+ }
+}
+//Ari Adrian Ibrahim 
+//1301142301
+//IF-38-01
